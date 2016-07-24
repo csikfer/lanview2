01fb2ccd0f0c06c1441f057b640642ab3b141c18
Csiki Ferenc <csikfer@gmail.com>
2016-07-24 19:35:03 +0200

    doc.

--- lv2/doxydoc.h
+++ lv2/doxydoc.h
@@ -8,11 +8,15 @@
 @mainpage LanView2 API
 @author Csiki Ferenc <csikfer@gmail.com>
 @section A LanView API Inicializálása, a lanView osztály
-Az API-nak hasonlóan a Qt-ben a QApplication osztályhoz van egy „globálisan” használt objektuma,
+Az API-nak hasonlóan a Qt-ben a QApplication osztályhoz van egy „globálisan” objektuma,
 amit egy példányban mindig létre kell hozni. A példány létrehozásakor inicializáljuk az API-t,
-és ez a példány teszi lehetővé pl. az adatbázis elérését.
+és ez a példány teszi lehetővé pl. az adatbázis elérését is.
 Bár a lanview osztály példányosítható, de praktikusan egy leszármazottját használjuk mely
-tartalmazhatja a program inicializálását is.
+tartalmazza a program inicializálását is.
+@note
+GUI applikáció esetén a lanView helyett az lv2g osztályt (vagy leszármazotját) kell példányosítni a
+lanView helyett, mely a lanView leszármazottja, és tartalmaz néhány GUI-val kapcsolatos kiegészítést.
+
 A lanView osztály példányosításakor a következő műveleteket hajta végre:
 - Betölti a nyelvi fájlokat a LANG környezeti változó alapján, ha vannak.
 - Betölti a config állományt a QSettings példányosításával.
@@ -23,14 +27,15 @@ A lanView osztály példányosításakor a következő műveleteket hajta végre
 - Inicializálja az SNMP alrendszert, ha kértük (ez Windows alatt hiányzik az API-ból).
 
 @section cError Hiba kezelés, a cError osztály.
-Az API hiba esetén egy a hiba paramétereivel létrehozott cError objektum pionterével hibát dob. Ha el akarjuk kapni a hibát, akkor az ilyenkor használatos try { … } blokkban használhatjuk a CATCHS() makrót, mely az összes kizárás típust lekezeli.
+Az API hiba esetén egy a hiba paramétereivel létrehozott cError objektum pionterével hibát dob. Ha el akarjuk kapni a hibát,
+akkor az ilyenkor használatos try { … } blokkban használhatjuk a CATCHS() makrót, mely az összes kizárás típust lekezeli.
 Program hiba esetén a következő makrók használhatóak:
 - EXCEPTION()	Általános hiba eseténi kizárást generáló makró
 - NEWCERROR()	Egy hiba objektum példány létrehozása, feltöltése.
 - LSTXGET()	Egy listából egy érték lekérése, hiba estén kizárás dobása.
 - LSTUXGET()	Egy listából egy érték lekérése, hiba estén kizárás dobása, az index előjel nélküli.
-- SQLERR()	SQL hiba
-- SQLOERR()	SQL megnyitási hiba
+- SQLERR()      SQL hiba
+- SQLOERR()     SQL megnyitási hiba
 - SQLPREPERR()	SQL prepare hiba
 - SQLQUERYERR()	SQL lekédezés hiba
 
@@ -40,7 +45,7 @@ A cDebug osztállyal közvetlenül nem találkozunk, azt a lanView osztály inic
 Használata pedig makrókon keresztül történik.
 Minden a cDebug-on keresztül kiírt üzenetet a cDebug egy fejléccel lát el, mely hasonló a syslog üzenetekhez,
 továbbá ha a program több szálú, akkor a fejléc tartalmazza az üzenetet küldő szál nevét is.
-A cDebug lehetőséget biztosít GUI program esetén az üzenetek elfogására a signal-on keresztül, ha azt a GUI-ban meg akarjuk jeleníteni.
+A cDebug lehetőséget biztosít GUI program esetén az üzenetek elfogására egy signal-on keresztül, ha azt a GUI-ban meg akarjuk jeleníteni.
 A cDebug az üzeneteket mindig a fő szálban írja ki, a mellék szálak üzeneteit egy QQueue soron keresztül kapja meg.
 Minden üzenet csak akkor kerül kiírásra ill. a sorba, ha lezártuk az endl manipulátorral.
 
@@ -50,7 +55,7 @@ Az adatbázis eléréséhez a newQuery() vagy a getQuery() függvényekkel kérh
 Az igy kapott objektumnál csak arra kell vigyázni, hogy abban a szálban kell hívni a fenti függvényeket, amelyikben
 használni is fogjuk a QSqlQuery objektumot.
 Az adatbázis eléréséhez az API nem definiál külön osztályt a magasabb szintű adat manipulátorokon kívül. De a következő
-függvényeket tartalmazza az adatkezelés megkönnyítésére:
+függvényeket tartalmazza az adatkezelés megkönnyítésére (lásd még: lv2sql.h):
 - QSqlDatabase *  getSqlDb(void);
 - static inline QSqlQuery * newQuery(QSqlDatabase *pDb)
 - static inline QSqlQuery getQuery(QSqlDatabase *pDb)
@@ -60,9 +65,12 @@ függvényeket tartalmazza az adatkezelés megkönnyítésére:
 - bool sqlEnd(QSqlQuery& q, enum eEx __ex)
 - bool sqlRollback(QSqlQuery& q, enum eEx __ex)
 - bool execSql(QSqlQuery& q, const QString& sql, const QVariant v1, const QVariant v2, const QVariant v3, const QVariant v4, const QVariant v5);
-- bool execSqlFunction(QSqlQuery& q, const QString& fn, const QVariant v1, const QVariant v2, const QVariant v3, const QVariant v4, const QVariant v5);
-- qlonglong execSqlIntFunction(QSqlQuery& q, bool *pOk, const QString& fn, const QVariant v1, const QVariant v2, const QVariant v3, const QVariant v4, const QVariant v5);
-- QString execSqlTextFunction(QSqlQuery& q, const QString& fn, const QVariant v1, const QVariant v2, const QVariant v3, const QVariant v4, const QVariant v5);
+- bool execSqlFunction(QSqlQuery& q, const QString& fn, const QVariant& v1, const QVariant& v2, const QVariant& v3, const QVariant& v4, const QVariant& v5);
+- qlonglong execSqlIntFunction(QSqlQuery& q, bool *pOk, const QString& fn, const QVariant& v1, const QVariant& v2, const QVariant& v3, const QVariant& v4, const QVariant& v5);
+- QString execSqlTextFunction(QSqlQuery& q, const QString& fn, const QVariant& v1, const QVariant& v2, const QVariant& v3, const QVariant& v4, const QVariant& v5);
+- bool execSqlRecFunction(QSqlQuery& q, const QString& fn, const QVariant& v1, const QVariant& v2, const QVariant& v3, const QVariant& v4, const QVariant& v5);
+- void sqlNotify(QSqlQuery& q, const QString& channel, const QString& payload);
+
 
 Továbbá néhány string függvény:
 - static inline QString mCat(const QString& a, const QString& b)
@@ -122,53 +130,62 @@ A cInspector osztály lekérdező programokban a host-services rekordok alapján
 a szolgáltatáspéldányok fáját, és vezérli a lekérdezés részfolyamatait. A konkrét résztevékenységek
 egy körét az alap osztály tartalmazza, illetve a cInspector származtatásával valósítható meg,
 a virtuális metódusokon keresztül. Egy cIspector objektum működését mindíg egy cHostServices objektum
-(vagyis egy hostt_services rekord) és az általuk hivatkozott cService objektumok (services rekord)
-határozzák meg. Kulcs szerepük ebben a features mezőknek van.
-@subsection windhcpconf
-Egy példa a windows szerver DHCP konfigurációjának a lekérdezéséhez.
-A lekérdezést az lv2d nevű APP Windows-os változata halythatja végre. A példában a lekérdezés a
+(vagyis egy hostt_services rekord) és az általuk hivatkozott cService objektumok (services rekordok)
+határozzák meg. Kulcs szerepük ebben még a features mezőknek is.
+@subsection windhcpconf Egy példa a windows szerver DHCP konfigurációjának a lekérdezéséhez.
+A lekérdezést az lv2d nevű APP Windows-os változata hajthatja végre. A példában a lekérdezés a
 netsh parancs hívásával valósul meg.
 A sükséges services definíciók (lásd: import_srv.txt fájlt, a példa ezzel nem azonos, kimaradtak a példa
 szempontjából irreleváns paraméterek):
 ~~~~~~~~~~
 SERVICE lv2d "Lanview2 super server" {
-    SUPERIOR SERVICE MASK "~."; // Semmire sem illeszkedik, csak gyökér lehet
-    FEATURES ":logrot=500M,8:";
+    NIL;
+    SUPERIOR SERVICE MASK "~."; // Semmire sem illeszkedik
+    FEATURES ":superior:timing=passive:logrot=500M,8:";
+    NORMAL CHECK INTERVAL  300;
 }
-SERVICE "win.dhcp.conf.parser" "A netsh dhcp kimenetének az értelmezése" {
+
+SERVICE "win.dhcp.conf.parser" "DHCP konfiguráció a netsh-n keresztül; Fordító" {
     FEATURES ":timing=polling:method=parser:";
 }
+
+SERVICE "win.dhcp.conf" "DHCP konfiguráció a netsh-n keresztül" {
+    FEATURES ":timing=polling:method=qparse:comment=#:";
+    CMD "netsh dhcp server \\\\$node dump all";
+}
+
 QUERY PARSER "win.dhcp.conf.parser" {
     CASE NO
         $rexp$Dhcp\s+Server\s+\\+([\w\-\.]+)\sScope\s+[\d\.\:]+\s+Add\s+iprange\s+([\d\.\:]+)\s+([\d\.\:]+)$rexp$
-        $ipar$REPLACE DYNAMIC ADDRESS RANGE $2 TO $3 $host_service_id;$ipar$;
+        $ipar$REPLACE DYNAMIC ADDRESS RANGE $2 TO $3 "$1".dhcp;$ipar$;
     CASE NO
         $rexp$Dhcp\s+Server\s+\\+([\w\-\.]+)\sScope\s+[\d\.\:]+\s+Add\s+excluderange\s+([\d\.\:]+)\s+([\d\.\:]+)$rexp$
-        $ipar$REPLACE DYNAMIC ADDRESS RANGE EXCLUDE $2 TO $3 $host_service_id;$ipar$;
+        $ipar$REPLACE DYNAMIC ADDRESS RANGE EXCLUDE $2 TO $3 "$1".dhcp;$ipar$;
     CASE NO
         $rexp$Dhcp\s+Server\s+\\+([\w\-\.]+)\sScope\s+[\d\.\:]+\s+Add\s+reservedip\s+([\d\.\:]+)\s+([\dA-F]+)\s+(.*)$rexp$
         $ipar$REPLACE ARP $2 MAC("$3") config $host_service_id $$note$$$4$$note$$;$ipar$;
 }
-SERVICE "win.dhcp.conf" "Windows DHCP konfiguráció lekérdezés" {
-    COMMAND "netsh dhcp server \\$node dump all";
-    FEATURES ":timing=polling:method=qparse:";
-}
-HOST SERVICE "query-host".lv2d {
+
+HOST SERVICE "win-lv2".lv2d {
     FEATURES ":timing=polling:";
 }
-HOST SERVICE "query-host"."win.dhcp.conf.parser" {
-    FEATURES ":superior:";
-    SUPERIOR SERVICE "query-host".lv2d;
+HOST SERVICE "win-lv2"."win.dhcp.conf.parser" {
+    SUPERIOR SERVICE "win-lv2".lv2d;
 }
-HOST SERVICE "dhcp-server1"."win.dhcp.conf" {
+HOST SERVICE "win-dhcp-1"."win.dhcp.conf" {
+    SUPERIOR SERVICE "win-lv2"."win.dhcp.conf.parser";
     FEATURES ":timing=polling:";
-    SUPERIOR "query-host"."win.dhcp.conf.parser";
 }
-HOST SERVICE "dhcp-server2"."win.dhcp.conf" {
+HOST SERVICE "win-dhcp-2"."win.dhcp.conf" {
+    SUPERIOR SERVICE "win-lv2"."win.dhcp.conf.parser";
     FEATURES ":timing=polling:";
-    SUPERIOR "query-host"."win.dhcp.conf.parser";
 }
 ~~~~~~~~~~
+A fenti példában a "win-lv2" nevű gép futtatj az lv2d appot, időzítés nélkül (idítás után egyszer elvégzi a feladatát majd kilép).
+A "win.dhcp.conf.parser" definiálja a módszert a lekérdezéshez a query_parser rekord definíciókkal együtt.
+A konkrét lekérdezés az utolsó két bejegyzés, két DHCP serverre, a win-dhcp-1 és win-decp-2 nevű szerver lekérdezése.
+Mivel a "win.dhcp.conf"-ben szerepel a gép név mint a netsh paraméter, ezért itt a lekérdezendő szerver nevének
+anonosnak kell lennie a Windows host névvel.
 
 @section timeper Idő intervallumok
 
