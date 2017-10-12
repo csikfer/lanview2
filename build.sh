700c0ceb502d819653549d1c21bdfc278c3f0c15
Csiki Ferenc <csikfer@gmail.com>
2017-10-12 14:05:18 +0200

    Régi okafogyott scriptek törlése.

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-APP=$1
-
-cd $APP
-qmake ${APP}.pro
-make
-cd ..
-
--- rebuild_all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-for a in lv2 lv2g import lv2d lv2gui arpd portmac portstat updt_oui ; do ./rebuild.sh $a ; done
-
