c1c87fb4baee4ad4b7edf28c487c41e0e7170ff9
Csiki Ferenc <csikfer@gmail.com>
2018-06-22 13:31:14 +0200

    Hibajavítás..

---
+++ database/update-1.13a.sql
@@ -0,0 +1,21 @@
+-- Hibajavítás
+
+-- Nem lehet törölni sunet-et
+CREATE OR REPLACE FUNCTION subnet_delete_before() RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE ip_addresses SET subnet_id = NULL, address = NULL WHERE subnet_id = OLD.subnet_id;
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER subnet_delete_beforeí_trigger BEFORE DELETE ON subnets FOR EACH ROW EXECUTE PROCEDURE subnet_delete_before();
+
+-- A VLAN-ok szintén törölhetetlenek
+CREATE OR REPLACE FUNCTION vlan_delete_before() RETURNS TRIGGER AS $$
+BEGIN
+    DELETE FROM subnets WHERE vlan_id = OLD.vlan_id;
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER vlan_delete_before_trigger BEFORE DELETE ON vlans FOR EACH ROW EXECUTE PROCEDURE vlan_delete_before();
